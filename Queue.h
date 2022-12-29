75ca85309a37d5fadf1079bc45f77be608918887
ToneGabriel <gabitone1998@gmail.com>
2022-12-29 16:24:40 +0200

    Update Queue.h

--- Queue.h
+++ Queue.h
@@ -3,20 +3,20 @@
 
 // Queue Node ========================================================
 template<class Queue>
-struct QueueNode                                                             // Struct that holds data and references to next struct
+struct QueueNode                                                     // Struct that holds data and references to next struct
 {
 private:
 	using ValueType = typename Queue::ValueType;
-	using Alloc		= typename Allocator<ValueType>;
+	using Alloc	    = typename Allocator<ValueType>;
 
 	Alloc _alloc;
 
 public:
-	ValueType Value;                                                         // Data
-	QueueNode* Next = nullptr;                                               // Reference to next
+	ValueType Value;                                                 // Data
+	QueueNode* Next = nullptr;                                       // Reference to next
 
 	template<class... Args>
-	QueueNode(Args&&... args) {                                              // Add data using emplace type Constructor
+	QueueNode(Args&&... args) {                                      // Add data using emplace type Constructor
 		_alloc.construct(&Value, std::forward<Args>(args)...);
 	}
 };
@@ -29,16 +29,16 @@ template<class Type>
 class Queue
 {
 public:
-	using ValueType = Type;                                                 // Type for stored values
-	using Node = typename QueueNode<Queue<ValueType>>;                      // Node type
+	using ValueType = Type;                                         // Type for stored values
+	using Node      = typename QueueNode<Queue<ValueType>>;         // Node type
 
 private:
-	size_t _size	= 0;													// Number of Nodes held by this
-	Node* _head		= nullptr;												// Head of this list
-	Node* _tail		= nullptr;												// Tail of this list
+	size_t _size    = 0;                                            // Number of Nodes held by this
+	Node* _head	    = nullptr;                                      // Head of this list
+	Node* _tail	    = nullptr;                                      // Tail of this list
 
-	mutable Node* _workspaceNode = nullptr;                                 // Auxiliary Node for work
-	mutable ValueType _workspaceValue;                                      // Auxiliary Value for work
+	mutable Node* _workspaceNode = nullptr;                         // Auxiliary Node for work
+	mutable ValueType _workspaceValue;                              // Auxiliary Value for work
 
 public:
 	// Constructors
@@ -46,27 +46,27 @@ public:
 	Queue() = default;
 
 	template<class... Args>
-	Queue(const size_t& newSize, Args&&... args) {                          // Emplace ValueType Constructor
+	Queue(const size_t& newSize, Args&&... args) {                  // Emplace ValueType Constructor
 		create_until_size(newSize, std::forward<Args>(args)...);
 	}
 
-	Queue(const size_t& newSize, const ValueType& value) {                  // Reference type Constructor
+	Queue(const size_t& newSize, const ValueType& value) {          // Reference type Constructor
 		create_until_size(newSize, value);
 	}
 
-	Queue(const size_t& newSize, ValueType&& value) {                       // Temporary type Constructor
+	Queue(const size_t& newSize, ValueType&& value) {               // Temporary type Constructor
 		create_until_size(newSize, std::move(value));
 	}
 
-	Queue(const Queue& other) {                                            // Copy Constructor
+	Queue(const Queue& other) {                                     // Copy Constructor
 		copy(other);
 	}
 
-	Queue(Queue<ValueType>&& other) noexcept {                              // Move Constructor
+	Queue(Queue<ValueType>&& other) noexcept {                      // Move Constructor
 		move(std::move(other));
 	}
 
-	~Queue() {                                                             // Destructor
+	~Queue() {                                                      // Destructor
 		clear();
 	}
 
@@ -74,19 +74,19 @@ public:
 	// Main functions
 
 	template<class... Args>
-	void enqueue(Args&&... args) {                                         // Construct object using arguments (Args) and add it to the tail
+	void enqueue(Args&&... args) {                                  // Construct object using arguments (Args) and add it to the tail
 		emplace_back(std::forward<Args>(args)...);
 	}
 
-	void enqueue(const ValueType& copyValue) {                             // Construct object using reference and add it to the tail
+	void enqueue(const ValueType& copyValue) {                      // Construct object using reference and add it to the tail
 		emplace_back(copyValue);
 	}
 
-	void enqueue(ValueType&& moveValue) {                                  // Construct object using temporary and add it to the tail
+	void enqueue(ValueType&& moveValue) {                           // Construct object using temporary and add it to the tail
 		emplace_back(std::move(moveValue));
 	}
 
-	ValueType&& dequeue() {                                                // Return first component and remove it from queue
+	ValueType&& dequeue() {                                         // Return first component and remove it from queue
 		if (_head) {
 			_workspaceValue = std::move(_head->Value);
 			_workspaceNode = _head;
@@ -102,15 +102,15 @@ public:
 		}
 	}
 
-	const size_t size() const {                                          // Get size
+	const size_t size() const {                                     // Get size
 		return _size;
 	}
 
-	bool empty() const {                                                 // Check if list is empty
+	bool empty() const {                                            // Check if list is empty
 		return _size == 0;
 	}
 
-	void clear() {                                                       // Remove ALL components
+	void clear() {                                                  // Remove ALL components
 		while (_size) {
 			_workspaceNode = _head;
 			_head = _head->Next;
@@ -125,14 +125,14 @@ public:
 public:
 	// Operators
 
-	Queue& operator=(const Queue<ValueType>& other) {                   // Assign operator using reference
+	Queue& operator=(const Queue<ValueType>& other) {              // Assign operator using reference
 		clear();
 		copy(other);
 
 		return *this;
 	}
 
-	Queue& operator=(Queue<ValueType>&& other) noexcept {               // Assign operator using temporary
+	Queue& operator=(Queue<ValueType>&& other) noexcept {          // Assign operator using temporary
 		clear();
 		move(std::move(other));
 
@@ -160,7 +160,7 @@ private:
 	}
 
 	template<class... Args>
-	void emplace_back(Args&&... args) {                                 // Construct object using arguments (Args) and add it to the tail
+	void emplace_back(Args&&... args) {                             // Construct object using arguments (Args) and add it to the tail
 		Node* newNode = new Node(std::forward<Args>(args)...);
 
 		if (_head == nullptr)
@@ -173,9 +173,9 @@ private:
 	}
 
 	template<class... Args>
-	void create_until_size(const size_t& newSize, Args&&... args) {			 // Add elements until current size equals newSize
+	void create_until_size(const size_t& newSize, Args&&... args) { // Add elements until current size equals newSize
 		while (_size < newSize)
-			emplace_back(std::forward<Args>(args)...);                       // Emplace type addition
+			emplace_back(std::forward<Args>(args)...);              // Emplace type addition
 	}
 };
 // Queue ========================================================
