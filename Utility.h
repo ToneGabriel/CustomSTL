660c5be7d020d977acfd2989fa451006113715e0
ToneGabriel <gabitone1998@gmail.com>
2023-02-15 19:24:50 +0200

    Update Utility.h

--- Utility.h
+++ Utility.h
@@ -5,34 +5,44 @@
 CUSTOM_BEGIN
 
 // remove reference
-template<class T>
-struct RemoveReference { using Type = T; };
+template<class Ty>
+struct RemoveReference { using Type = Ty; };
 
-template<class T>
-struct RemoveReference<T&> { using Type = T; };
+template<class Ty>
+struct RemoveReference<Ty&> { using Type = Ty; };
 
-template<class T>
-struct RemoveReference<T&&> { using Type = T; };
+template<class Ty>
+struct RemoveReference<Ty&&> { using Type = Ty; };
 
+// is_lvalue_reference
+template <class>
+constexpr bool is_lvalue_reference = false;   // determine whether type argument is an lvalue reference
 
+template <class Ty>
+constexpr bool is_lvalue_reference<Ty&> = true;
 
 // Class template IntegerSequence
-template<class T, T... Idx>
+template<class Ty, Ty... Vals>
 struct IntegerSequence
 {
-    using ValueType = T;
-    static constexpr size_t size() noexcept { return sizeof...(Idx); }
+    using ValueType = Ty;
+    static constexpr size_t size() noexcept { return sizeof...(Vals); }
 };
 
 // Aliases
-template<size_t... Idx>
-using IndexSequence = IntegerSequence<size_t, Idx...>;
+template<size_t... Vals>
+using IndexSequence = IntegerSequence<size_t, Vals...>;
 
-template<class T, T Num>
-using MakeIntegerSequence = IntegerSequence<T, __integer_pack(Num)...>;
+template<class Ty, Ty Size>
+#ifdef __make_integer_seq   // TODO: check
+using MakeIntegerSequence = __make_integer_seq<IntegerSequence, Ty, Size>;
+#else
+using MakeIntegerSequence = __make_integer_seq<IntegerSequence, Ty, Size>;
+//using MakeIntegerSequence = IntegerSequence<Ty, __integer_pack(Size)...>;
+#endif // __make_integer_seq
 
-template<size_t Num>
-using MakeIndexSequence = MakeIntegerSequence<size_t, Num>;
+template<size_t Size>
+using MakeIndexSequence = MakeIntegerSequence<size_t, Size>;
 
 template<class... Types>
 using IndexSequenceFor = MakeIndexSequence<sizeof...(Types)>;
@@ -40,22 +50,21 @@ using IndexSequenceFor = MakeIndexSequence<sizeof...(Types)>;
 
 
 // move
-template<class T>
-constexpr typename RemoveReference<T>::Type&& move(T&& obj) noexcept { 
-    return static_cast<typename RemoveReference<T>::Type&&>(obj);
+template<class Ty>
+constexpr typename RemoveReference<Ty>::Type&& move(Ty&& val) noexcept { 
+    return static_cast<typename RemoveReference<Ty>::Type&&>(val);
 }
 
 // forward
-template<class T>
-constexpr T&& forward(typename RemoveReference<T>::Type& obj) noexcept { 
-    return static_cast<T&&>(obj);
+template<class Ty>
+constexpr Ty&& forward(typename RemoveReference<Ty>::Type& val) noexcept { 
+    return static_cast<Ty&&>(val);
 }
 
-template<class T>
-constexpr T&& forward(typename RemoveReference<T>::Type&& obj) noexcept {
-    // static_assert(!std::is_lvalue_reference<T>::value, "template argument"
-    //     " substituting T must not be an lvalue reference Type");
-    return static_cast<T&&>(obj);
+template<class Ty>
+constexpr Ty&& forward(typename RemoveReference<Ty>::Type&& val) noexcept {
+    static_assert(!is_lvalue_reference<Ty>, "bad forward call");
+    return static_cast<Ty&&>(val);
 }
 
 CUSTOM_END
