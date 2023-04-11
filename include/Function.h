3e58cdb08433bc6e536d5aeb18f7fab81a780dd6
ToneGabriel <gabitone1998@gmail.com>
2023-04-11 18:50:14 +0300

    Update Function.h

--- include/Function.h
+++ include/Function.h
@@ -1,6 +1,5 @@
 #pragma once
 #include "Utility.h"
-#include <iostream>
 
 CUSTOM_BEGIN
 
@@ -24,19 +23,19 @@ private:
 private:
     // Core functions
 
-    template<class FuncType>
-    static RetType _invoke(const Function& target, ArgTypes&&... args) {
-        return (*reinterpret_cast<FuncType*>(target._functor))(custom::forward<ArgTypes>(args)...);
+    template<class Functor>
+    static RetType _invoke_impl(const Function& target, ArgTypes&&... args) {
+        return (*reinterpret_cast<Functor*>(target._functor))(custom::forward<ArgTypes>(args)...);
     }
 
-    template<class FuncType>
-    static void* _copy(const Function& target) {
-        return reinterpret_cast<void*>(new FuncType(*reinterpret_cast<FuncType*>(target._functor)));
+    template<class Functor>
+    static void* _copy_impl(const Function& target) {
+        return reinterpret_cast<void*>(new Functor(*reinterpret_cast<Functor*>(target._functor)));
     }
 
-    template<class FuncType>
-    static void _destruct(const Function& target) {
-        delete reinterpret_cast<FuncType*>(target._functor);
+    template<class Functor>
+    static void _destruct_impl(const Function& target) {
+        delete reinterpret_cast<Functor*>(target._functor);
     }
 
 public:
@@ -44,36 +43,26 @@ public:
 
     Function() = default;
 
-    template<class FuncType, custom::EnableIf_t<!custom::IsSame<custom::Decay_t<FuncType>, Function>::Value, bool> = true>
-    Function(FuncType&& f)
-        :   _invoker(reinterpret_cast<Invoker>(_invoke<custom::Decay_t<FuncType>>)),
-            _copiator(reinterpret_cast<Copiator>(_copy<custom::Decay_t<FuncType>>)),
-            _destructor(reinterpret_cast<Destructor>(_destruct<custom::Decay_t<FuncType>>)),
-            _functor(reinterpret_cast<void*>(new custom::Decay_t<FuncType>(custom::forward<FuncType>(f))))
-            { /*Empty*/ }
+    template<class Functor, custom::EnableIf_t<!custom::IsSame<custom::Decay_t<Functor>, Function>::Value, bool> = true>
+    Function(Functor&& f) {
+        using FunctorType = custom::Decay_t<Functor>;
 
-    Function(const Function& other)
-        :   _invoker(other._invoker),
-            _copiator(other._copiator),
-            _destructor(other._destructor),
-            _functor(other._copiator(other))
-            { /*Empty*/ }
+        _invoker    = reinterpret_cast<Invoker>(_invoke_impl<FunctorType>);
+        _copiator   = reinterpret_cast<Copiator>(_copy_impl<FunctorType>);
+        _destructor = reinterpret_cast<Destructor>(_destruct_impl<FunctorType>);
+        _functor    = reinterpret_cast<void*>(new FunctorType(custom::forward<Functor>(f)));
+    }
+
+    Function(const Function& other) {
+        _copy(other);
+    }
 
     Function(Function&& other) noexcept {
-        _invoker            = other._invoker;
-        _copiator           = other._copiator;
-        _destructor         = other._destructor;
-        _functor            = other._functor;
-
-        other._invoker      = nullptr;
-        other._copiator     = nullptr;
-        other._destructor   = nullptr;
-        other._functor      = nullptr;
+        _move(custom::move(other));
     }
 
     ~Function() {
-        if (_functor != nullptr)
-            _destructor(*this);
+        _clear();
     }
     
 public:
@@ -87,38 +76,57 @@ public:
     }
 
     Function& operator=(const Function& other) {
-        if (this != &other)
+        if (_functor != other._functor)
         {
-            if (_functor != nullptr)
-                _destructor(*this);
-
-            _invoker    = other._invoker;
-            _copiator   = other._copiator;
-            _destructor = other._destructor;
-            _functor    = other._copiator(other);
+            _clear();
+            _copy(other);
         }
 
         return *this;
     }
 
     Function& operator=(Function&& other) {
-        if (this != &other)
+        if (_functor != other._functor)
         {
-            if (_functor != nullptr)
-                _destructor(*this);
-
-            _invoker            = other._invoker;
-            _copiator           = other._copiator;
-            _destructor         = other._destructor;
-            _functor            = other._functor;
-
-            other._invoker      = nullptr;
-            other._copiator     = nullptr;
-            other._destructor   = nullptr;
-            other._functor      = nullptr;
+            _clear();
+            _move(custom::move(other));
         }
 
         return *this;
+    }
+
+    explicit operator bool() const noexcept {
+        if (_functor != nullptr)
+            return true;
+
+        return false;
+    }
+
+private:
+    // Helpers
+
+    void _clear() {
+        if (_functor != nullptr)
+        {
+            _destructor(*this);
+            _functor = nullptr;
+        }
+    }
+
+    void _copy(const Function& other) {
+        _invoker        = other._invoker;
+        _copiator       = other._copiator;
+        _destructor     = other._destructor;
+        _functor        = other._copiator(other);
+    }
+
+    void _move(Function&& other) {
+        _invoker        = other._invoker;
+        _copiator       = other._copiator;
+        _destructor     = other._destructor;
+        _functor        = other._functor;
+
+        other._functor  = nullptr;
     }
 }; // END Function template
 
