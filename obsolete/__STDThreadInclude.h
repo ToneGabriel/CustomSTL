6f3876ae9600f5ea27fa7925c2f397b00e3d579d
ToneGabriel <81569579+ToneGabriel@users.noreply.github.com>
2025-06-30 09:35:37 +0300

    Update

--- obsolete/__STDThreadInclude.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <thread>
-#include <mutex>
-#include <condition_variable>
-#include <semaphore>
-#include <barrier>
-#include <shared_mutex>
---
+++ test_c/test_priority_queue.c
@@ -0,0 +1,6 @@
+#include "unity.h"
+#include "priority_queue.h"
+
+
+DEFINE_PRIORITY_QUEUE(TestPriorityQueue, int)
+
