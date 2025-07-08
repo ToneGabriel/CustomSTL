ba35d29a14d55abb163a4ed851b4c7e667d25fea
ToneGabriel <81569579+ToneGabriel@users.noreply.github.com>
2025-07-08 16:53:37 +0300

    Update

---
+++ custom_stl_c/test/c_priority_queue_test.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "stl_c/c_priority_queue.h"
+
+
+DEFINE_PRIORITY_QUEUE(IntPQ, double)
+
+
+TEST(PriorityQueueTest, default_init)
+{
+    IntPQ* pq = IntPQ_create(10);
+    IntPQ_destroy(pq);
+}
