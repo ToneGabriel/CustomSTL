da12dba034ef7c5b46753bf3523d458ce8e9e693
ToneGabriel <81569579+ToneGabriel@users.noreply.github.com>
2025-08-05 21:03:14 +0300

    Create _c_node.h

---
+++ custom_stl_c/include/custom/_c_node.h
@@ -0,0 +1,18 @@
+#ifndef _C_NODE_H
+#define _C_NODE_H
+
+
+#include "custom/_c_core.h"
+
+
+#define DEFINE_SINGLE_NODE(     \
+    SINGLE_NODE_NAME,           \
+    TYPE,                       \
+    TYPE_REF_DELETE_FUNC        \
+)                               \
+
+
+#define DEFINE_DOUBLE_NODE
+
+
+#endif  // _C_NODE_H
