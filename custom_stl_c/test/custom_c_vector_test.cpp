d3a1269a62222849d240d4fe5d68e2c5d07c62a3
ToneGabriel <81569579+ToneGabriel@users.noreply.github.com>
2025-08-11 12:18:55 +0300

    Update c_vector test

--- custom_stl_c/test/custom_c_vector_test.cpp
+++ custom_stl_c/test/custom_c_vector_test.cpp
@@ -13,8 +13,60 @@ DEFINE_GENERIC_VECTOR(
 )
 
 
-TEST(VectorTest, default_init)
+class CVectorValueTestFixture : public ::testing::TestWithParam<int>
 {
-    VectorINT vec = VectorINT_create(10);
-    VectorINT_destroy(&vec);
+protected:
+    VectorINT _custom_vector_instance;
+
+protected:
+
+    void SetUp() override
+    {
+        _custom_vector_instance = VectorINT_create(10);
+    }
+
+    void TearDown() override
+    {
+        VectorINT_destroy(&_custom_vector_instance);
+    }
+};  // END CVectorValueTestFixture
+
+
+TEST_F(CVectorValueTestFixture, default_create)
+{
+    EXPECT_EQ(VectorINT_size(&this->_custom_vector_instance), 0);
+    EXPECT_EQ(VectorINT_capacity(&this->_custom_vector_instance), 10);
+    EXPECT_TRUE(VectorINT_empty(&this->_custom_vector_instance));
+}
+
+TEST_F(CVectorValueTestFixture, copy)
+{
+    int val = 0;
+    VectorINT_push_back(&this->_custom_vector_instance, &val);
+    VectorINT_push_back(&this->_custom_vector_instance, &val);
+    VectorINT_push_back(&this->_custom_vector_instance, &val);
+
+    VectorINT other = VectorINT_create(10);
+    EXPECT_FALSE(VectorINT_equals(&other, &this->_custom_vector_instance));
+
+    VectorINT_copy(&other, &this->_custom_vector_instance);
+    EXPECT_TRUE(VectorINT_equals(&other, &this->_custom_vector_instance));
+}
+
+TEST_F(CVectorValueTestFixture, move)
+{
+    int val = 0;
+    VectorINT_push_back(&this->_custom_vector_instance, &val);
+    VectorINT_push_back(&this->_custom_vector_instance, &val);
+    VectorINT_push_back(&this->_custom_vector_instance, &val);
+
+    VectorINT original_copy = VectorINT_create(1);
+    VectorINT_copy(&original_copy, &this->_custom_vector_instance);
+
+    VectorINT other = VectorINT_create(10);
+    EXPECT_FALSE(VectorINT_equals(&other, &this->_custom_vector_instance));
+
+    VectorINT_move(&other, &this->_custom_vector_instance);
+    EXPECT_FALSE(VectorINT_equals(&other, &this->_custom_vector_instance));
+    EXPECT_TRUE(VectorINT_equals(&other, &original_copy));
 }
